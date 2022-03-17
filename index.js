1d28e4e41377bac3280992adac74bf81f61e0a38
adrian.ganga <adiganga2002@gmail.com>
2022-03-17 13:25:44 +0200

    #595 Refactoring epi-util (moved to gtin-resolver)

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ const GtinDSUFactory = require("./lib/GTIN_DSU_Factory");
 
 resolver.registerDSUFactory("gtin", new GtinDSUFactory(resolver));
 const {createGTIN_SSI, parseGTIN_SSI} = require("./lib/GTIN_SSI");
-const {getDisabledFeatures} = require("./lib/ServerFeatureManager");
+const {getDisabledFeatures} = require("./lib/DSUFabricFeatureManager");
 module.exports = {
   createGTIN_SSI,
   parseGTIN_SSI,
   getDisabledFeatures,
+  loadApi: function (apiName) {
+    switch (apiName) {
+      case "mappings":
+        return require("./lib/mappings");
+      case "services":
+        return require("./lib/services");
+    }
+  },
+  getEPIMappingEngineForAPIHUB: function (server) {
+    return require("./lib/apihubMappingEngine").getEPIMappingEngineForAPIHUB(server);
+  },
+  getEPIMappingEngineMessageResults: function (server) {
+    return require("./lib/apihubMappingEngineMessageResults").getEPIMappingEngineMessageResults(server);
+  },
+  getMessagesPipe: function () {
+    const opendsu = require("opendsu");
+    return opendsu.loadApi("m2dsu").getMessagesPipe();
+  },
+  getErrorsMap: function () {
+    return require("opendsu").loadApi("m2dsu").getErrorsMap();
+  },
+  getMappingsUtils: function () {
+    return require("./lib/utils/commonUtils");
+  }
 }
 
