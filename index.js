7662f5511b479e4c825938e66beb8fc6e4f56f77
adrian.ganga <adiganga2002@gmail.com>
2022-03-28 13:11:33 +0300

    #592 Create in gtin-resolver a factory of models for batches and products + clean-up + moved servicies in gtin-resolver

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const GtinDSUFactory = require("./lib/GTIN_DSU_Factory");
 
 resolver.registerDSUFactory("gtin", new GtinDSUFactory(resolver));
 const {createGTIN_SSI, parseGTIN_SSI} = require("./lib/GTIN_SSI");
-const {getDisabledFeatures} = require("./lib/DSUFabricFeatureManager");
+const DSUFabricFeatureManager = require("./lib/DSUFabricFeatureManager");
+const LeafletFeatureManager = require("./lib/LeafletFeatureManager");
+const LeafletInfoService = require("./lib/services/LeafletInfoService");
+const DSUFabricUtilsService = require("./lib/services/DSUFabricUtilsService");
+const Languages = require("./lib/constants/Languages");
+const UploadTypes = require("./lib/constants/UploadTypes");
+
 module.exports = {
   createGTIN_SSI,
   parseGTIN_SSI,
-  getDisabledFeatures,
+  DSUFabricFeatureManager,
+  LeafletFeatureManager,
+  LeafletInfoService,
+  DSUFabricUtilsService,
+  UploadTypes,
+  Languages,
   loadApi: function (apiName) {
     switch (apiName) {
       case "mappings":
