a5089b2752c8e913e31b181d9cb983bb0c299515
skutner <sava.dumitru.daniel@gmail.com>
2024-02-08 11:08:09 +0200

    axiologic-pla/dev#639 axiologic-pla/dev#640 Add EPISORClient to gtin-resolver index
    Added functionality to gtin-resolver/index.js, specifically getEPISorClient, enabling the retrieval of an instance of the EPISORClient. This new function allows access to domain and subdomain via the EpiSORIntegrationClient. The changes serve to broaden the scope of available integration APIs.

--- index.js
+++ index.js
@@ -19,57 +19,61 @@ const versionTransformer = require("./lib/EpiVersionTransformer")
 const constants = require("./lib/constants/constants");
 
 module.exports = {
-  createGTIN_SSI,
-  parseGTIN_SSI,
-  DSUFabricFeatureManager,
-  LeafletFeatureManager,
-  LeafletInfoService,
-  DSUFabricUtils,
-  UploadTypes,
-  Languages,
-  Countries,
-  utils,
-  logUtils,
-  validationUtils,
-  versionTransformer,
-  constants,
-  XMLDisplayService,
-  loadApi: function (apiName) {
-    switch (apiName) {
-      case "mappings":
-        return require("./lib/mappings");
-      case "services":
-        return require("./lib/services");
+    createGTIN_SSI,
+    parseGTIN_SSI,
+    DSUFabricFeatureManager,
+    LeafletFeatureManager,
+    LeafletInfoService,
+    DSUFabricUtils,
+    UploadTypes,
+    Languages,
+    Countries,
+    utils,
+    logUtils,
+    validationUtils,
+    versionTransformer,
+    constants,
+    XMLDisplayService,
+    loadApi: function (apiName) {
+        switch (apiName) {
+            case "mappings":
+                return require("./lib/mappings");
+            case "services":
+                return require("./lib/services");
+        }
+    },
+    getEPIMappingEngineForAPIHUB: function (server) {
+        return require("./lib/apihubMappingEngine").getEPIMappingEngineForAPIHUB(server);
+    },
+    getEPIMappingEngineMessageResults: function (server) {
+        return require("./lib/apihubMappingEngineMessageResults").getEPIMappingEngineMessageResults(server);
+    },
+    getWebLeaflet: function (server) {
+        return require("./lib/leaflet-web-api").getWebLeaflet(server);
+    },
+    getGTINOwner: function (server) {
+        return require("./lib/gtinOwner").getGTINOwner(server);
+    },
+    getMessagesPipe: function () {
+        const opendsu = require("opendsu");
+        return opendsu.loadApi("m2dsu").getMessagesPipe();
+    },
+    getErrorsMap: function () {
+        return require("opendsu").loadApi("m2dsu").getErrorsMap();
+    },
+    getMappingsUtils: function () {
+        return require("./lib/utils/CommonUtils");
+    },
+    getMockEPISORClient: function (domain) {
+        const MockEPISORClient = require("./lib/integrationAPIs/clients/MockClient");
+        return MockEPISORClient.getInstance(domain);
+    },
+    getEPISorClient: function (domain, subdomain) {
+        const EPISORClient = require("./lib/integrationAPIs/clients/EpiSORIntegrationClient");
+        return EPISORClient.getInstance(domain, subdomain);
+    },
+    getIntegrationAPIs: function (server) {
+        return require("./lib/integrationAPIs")(server);
     }
-  },
-  getEPIMappingEngineForAPIHUB: function (server) {
-    return require("./lib/apihubMappingEngine").getEPIMappingEngineForAPIHUB(server);
-  },
-  getEPIMappingEngineMessageResults: function (server) {
-    return require("./lib/apihubMappingEngineMessageResults").getEPIMappingEngineMessageResults(server);
-  },
-  getWebLeaflet: function (server) {
-    return require("./lib/leaflet-web-api").getWebLeaflet(server);
-  },
-  getGTINOwner: function (server){
-    return require("./lib/gtinOwner").getGTINOwner(server);
-  },
-  getMessagesPipe: function () {
-    const opendsu = require("opendsu");
-    return opendsu.loadApi("m2dsu").getMessagesPipe();
-  },
-  getErrorsMap: function () {
-    return require("opendsu").loadApi("m2dsu").getErrorsMap();
-  },
-  getMappingsUtils: function () {
-    return require("./lib/utils/CommonUtils");
-  },
-  getMockEPISORClient: function (domain) {
-    const MockEPISORClient = require("./lib/integrationAPIs/clients/MockClient");
-    return MockEPISORClient.getInstance(domain);
-  },
-  getIntegrationAPIs: function (server) {
-    return require("./lib/integrationAPIs")(server);
-  }
 }
 
