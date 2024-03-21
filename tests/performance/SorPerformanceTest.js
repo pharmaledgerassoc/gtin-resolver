8eadc3f16966c2828bf87fb142df05f9b0fb8df1
skutner <sava.dumitru.daniel@gmail.com>
2024-03-21 16:56:32 +0200

    Revised performance tests for EpiSOR Integration
    Performance tests in SorPerformanceTest.js have been revised. Revisions include changing repetitions in measureApiPerformance from 2 to 30 and adjusting various other test settings for improved coverage and accuracy. The environment setup and handling of api keys for the client have been also reworked for enhanced security and efficiency.

--- tests/performance/SorPerformanceTest.js
+++ tests/performance/SorPerformanceTest.js
@@ -9,7 +9,7 @@ const path = require("path");
 const fs = require("fs");
 
 // Function to measure execution time
-async function measureApiPerformance(apiFunction, repetitions = 2) {
+async function measureApiPerformance(apiFunction, repetitions = 30) {
     const start = Date.now();
     const promises = [];
 
@@ -29,10 +29,8 @@ async function measureApiPerformance(apiFunction, repetitions = 2) {
 
 assert.callback("EPISORClient Write API Performance Tests", async (callback) => {
     // Set up API Hub test node (same as in the provided test suite)
-    const domain = 'testDomain';
-    const subdomain = 'testSubdomain';
-    const gtin = '02113111111164';
-    const batchNumber = 'B123';
+    const domain = 'testnet.dev';
+    const subdomain = 'testnet.dev.epi';
     const language = 'en';
     const productDetails = {
         "messageType": "Product",
@@ -49,21 +47,7 @@ assert.callback("EPISORClient Write API Performance Tests", async (callback) => 
             "strength": ""
         }
     };
-    const productDetails2 = {
-        "messageType": "Product",
-        "messageTypeVersion": 1,
-        "senderId": "ManualUpload",
-        "receiverId": "QPNVS",
-        "messageId": "S000001",
-        "messageDateTime": "2023-01-11T09:10:01CET",
-        "payload": {
-            "productCode": "00000000000000",
-            "internalMaterialCode": "",
-            "inventedName": "BOUNTY",
-            "nameMedicinalProduct": "BOUNTY® 250 mg / 0.68 mL pre-filled syringe",
-            "strength": ""
-        }
-    };
+
     const batchDetails = {
         "messageType": "Batch",
         "messageTypeVersion": 1,
@@ -82,122 +66,65 @@ assert.callback("EPISORClient Write API Performance Tests", async (callback) => 
     const leafletDetails = require("../leaflet.json");
     const image = require("../image.json");
 
-    const vaultDomainConfig = {
-        "anchoring": {
-            "type": "FS",
-            "option": {}
-        },
-        "enable": ["enclave", "mq"]
-    }
-
-    const folder = await $$.promisify(dc.createTestFolder)("testFolder");
-    const serverConfig = {
-        "storage": folder,
-        "port": 8080,
-        "preventRateLimit": true,
-        "activeComponents": [
-            "bdns",
-            "bricking",
-            "anchoring",
-            "epi-mapping-engine",
-            "epi-mapping-engine-results",
-            "get-gtin-owner",
-            "leaflet-web-api",
-            "integration-api",
-            "mq",
-            "secrets",
-            "lightDBEnclave",
-            "staticServer"
-        ],
-        "componentsConfig": {
-            "epi-mapping-engine": {
-                "module": "./../../gtin-resolver",
-                "function": "getEPIMappingEngineForAPIHUB"
-            },
-            "epi-mapping-engine-results": {
-                "module": "./../../gtin-resolver",
-                "function": "getEPIMappingEngineMessageResults"
-            },
-            "leaflet-web-api": {
-                "module": "./../../gtin-resolver",
-                "function": "getWebLeaflet"
-            },
-            "get-gtin-owner": {
-                "module": "./../../gtin-resolver",
-                "function": "getGTINOwner"
-            },
-            "integration-api": {
-                "module": "./../../gtin-resolver",
-                "function": "getIntegrationAPIs"
-            },
-            "staticServer": {
-                "excludedFiles": [
-                    ".*.secret"
-                ]
-            },
-            "bricking": {},
-            "anchoring": {}
-        },
-        "enableSimpleAuth": true
-    }
-
     process.env.SSO_SECRETS_ENCRYPTION_KEY = "+WG9HhIoXGGSVq6cMlhy2P3vuiqz1O/WAaiF5JhXmnc=";
-    process.env.EPI_DOMAIN = domain;
-    process.env.EPI_SUBDOMAIN = subdomain;
-    process.env.PSK_CONFIG_LOCATION = require("path").join(folder, "external-volume/config");
+
     const openDSU = require("opendsu");
     const crypto = openDSU.loadAPI("crypto");
     const apiKeyAPIs = openDSU.loadAPI("apiKey");
     const keySSISpace = openDSU.loadAPI("keyssi");
     const http = openDSU.loadAPI("http");
+    const systemAPI = openDSU.loadAPI("system");
+    systemAPI.setEnvironmentVariable(openDSU.constants.BDNS_ROOT_HOSTS, "https://c1.dev.pladevs.com")
     const generateEncryptionKey = () => {
         return crypto.generateRandom(32).toString("base64");
     }
-    const htPasswordPath = path.join(folder, ".htpassword.secret");
-    for (let i = 0; i < 10; i++) {
-        const user = `user${i}`;
-        const password = `password${i}`;
-        const hashedPassword = crypto.sha256JOSE(password).toString("hex");
-        const mail = `usr${i}@example.com`;
-        const ssoId = `usr${i}@example.com`;
-        fs.appendFileSync(htPasswordPath, `${user}:${hashedPassword}:${mail}:${ssoId}\n`);
+
+    const tokenEndpoint = "https://login.microsoftonline.com/d69968dd-8f36-47eb-b724-7f5e6e660066/oauth2/v2.0/token";
+    const clientId = "5daf11d0-dc28-4d09-b8c7-2eec6f16eb78";
+    const clientSecret = "";
+    const scope = "api://5daf11d0-dc28-4d09-b8c7-2eec6f16eb78/.default";
+    const getAccessToken = async () => {
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        params.append('client_id', clientId);
+        params.append('client_secret', clientSecret);
+        params.append('scope', scope);
+
+        let response;
+        response = await fetch(tokenEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: params
+        })
+
+        let data = await response.json();
+        return data.access_token;
     }
 
+    const accessToken = await getAccessToken();
     const interceptor = (data, callback) => {
         let {url, headers} = data;
         if (!headers) {
             headers = {};
         }
 
-        if (!headers.authorization) {
-            headers.authorization = authorization;
+        if (!headers["x-api-key"]) {
+            headers["x-api-key"] = process.env.SSO_SECRETS_ENCRYPTION_KEY;
         }
+
+        if (!headers.authorization) {
+            headers.authorization = `Bearer ${accessToken}`;
+        }
+
         callback(undefined, {url, headers});
     }
+
     http.registerInterceptor(interceptor);
 
-    const {port} = await tir.launchConfigurableApiHubTestNodeAsync({
-        domains: [{name: domain, config: vaultDomainConfig}, {name: subdomain, config: vaultDomainConfig}],
-        rootFolder: folder,
-        serverConfig: serverConfig
-    });
-
-    const url = `http://localhost:${port}`;
-    const apiKeysClient = apiKeyAPIs.getAPIKeysClient(url);
-    const authorization = `Bearer user1:${crypto.sha256JOSE("password1").toString("hex")}`
-    const headers = {
-        "Authorization": authorization
-    }
-
-    const apiKey = generateEncryptionKey();
-    const body = {
-        apiKey,
-        secret: generateEncryptionKey()
-    }
-    await apiKeysClient.becomeSysAdmin(JSON.stringify(body), headers);
-    await apiKeysClient.associateAPIKey("DSU_Fabric", "name", "usr1@example.com", {secret: generateEncryptionKey(), scope:"write"}, headers);
-
-    const secretsService = await require("apihub").getSecretsServiceInstanceAsync(folder);
+    const apiHub = require("apihub");
+    const secretsService = await apiHub.getSecretsServiceInstanceAsync(path.join(apiHub.getServerConfig().storage, "../../apihub-root"));
     const LightDBEnclaveFactory = require("../../../gtin-resolver/lib/integrationAPIs/utils/LightDBEnclaveFactory");
     const lightDBEnclaveFactory = LightDBEnclaveFactory.getLightDBEnclaveFactoryInstance();
     const seedSSI = await $$.promisify(keySSISpace.createSeedSSI)(domain);
@@ -205,16 +132,17 @@ assert.callback("EPISORClient Write API Performance Tests", async (callback) => 
 
     const client = EpiSORIntegrationClient.getInstance(domain, subdomain);
     // promisify all methods of client
-    for(let prop in client) {
-        if(typeof client[prop] === 'function') {
+    for (let prop in client) {
+        if (typeof client[prop] === 'function') {
             client[prop] = $$.promisify(client[prop]);
         }
     }
     // --- Performance Tests ---
 // Test for addProduct
     console.log('Testing addProduct performance...');
-    const generatedGTINs = [];
-    const generatedBatchNumbers = [];
+    let existingGTINs = [];
+    let existingBatchNumbers = [];
+
     function generateRandomGTIN14() {
         // Generate the first 13 digits randomly
         let gtinValue = '';
@@ -240,7 +168,7 @@ assert.callback("EPISORClient Write API Performance Tests", async (callback) => 
         }
 
         gtinValue += validDigit;
-        generatedGTINs.push(gtinValue);
+        existingGTINs.push(gtinValue);
         return gtinValue;
     }
 
@@ -251,35 +179,48 @@ assert.callback("EPISORClient Write API Performance Tests", async (callback) => 
         for (let i = 0; i < length; i++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
-        generatedBatchNumbers.push(result);
         return result;
     }
 
-    const updateProduct = async ()=>{
+    const products = await client.listProducts();
+    existingGTINs = existingGTINs.concat(products.map(product => product.productCode));
+
+    const batches = await client.listBatches();
+    existingBatchNumbers = existingBatchNumbers.concat(batches.map(batch => batch.batchNumber));
+    let existingBatchObjects = batches.map(batch => {
+        return {
+            productCode: batch.productCode,
+            batchNumber: batch.batchNumber
+        }
+    });
+    const updateProduct = async () => {
         const gtin = generateRandomGTIN14();
+        productDetails.payload.inventedName = `Product ${generateRandomBatchNumber(5)}`;
+        productDetails.payload.nameMedicinalProduct = productDetails.payload.inventedName;
         productDetails.payload.productCode = gtin;
         await client.updateProduct(gtin, productDetails);
     }
 
     // Test for updateProduct
     console.log('Testing updateProduct performance...');
-    await measureApiPerformance(updateProduct.bind(client));
+    await measureApiPerformance(updateProduct);
 
 
     const updateBatch = async ()=>{
-        const gtin = generatedGTINs[Math.floor(Math.random() * generatedGTINs.length)];
+        const gtin = existingGTINs[Math.floor(Math.random() * existingGTINs.length)];
         const batchNumber = generateRandomBatchNumber();
         batchDetails.payload.productCode = gtin;
         batchDetails.payload.batchNumber = batchNumber;
+        existingGTINs.push(gtin);
         await client.updateBatch(gtin, batchNumber, batchDetails);
     }
     // Test for updateBatch
     console.log('Testing updateBatch performance...');
     await measureApiPerformance(updateBatch);
-    //
+
 
     const updateImage = async ()=>{
-        const gtin = generatedGTINs[Math.floor(Math.random() * generatedGTINs.length)];
+        const gtin = existingGTINs[Math.floor(Math.random() * existingGTINs.length)];
         image.payload.productCode = gtin;
         await client.updateImage(gtin, image);
     }
@@ -288,24 +229,23 @@ assert.callback("EPISORClient Write API Performance Tests", async (callback) => 
     await measureApiPerformance(updateImage);
 
     // Test for updateProductEPI
-    const updateProductEPI = async ()=>{
-        const gtin = generatedGTINs[Math.floor(Math.random() * generatedGTINs.length)];
+    const updateProductEPI = async () => {
+        const gtin = existingGTINs[Math.floor(Math.random() * existingGTINs.length)];
         leafletDetails.payload.productCode = gtin;
         await client.updateProductEPI(gtin, language, "leaflet", leafletDetails);
     }
     console.log('Testing updateProductEPI performance...');
-    await measureApiPerformance(updateProductEPI);
+    await measureApiPerformance(updateProductEPI, 20);
 
     const updateBatchEPI = async ()=>{
-        const gtin = generatedGTINs[Math.floor(Math.random() * generatedGTINs.length)];
-        const batchNumber = generatedBatchNumbers[Math.floor(Math.random() * generatedBatchNumbers.length)];
-        leafletDetails.payload.productCode = gtin;
-        leafletDetails.payload.batchNumber = batchNumber;
-        await client.updateBatchEPI(gtin, batchNumber, language, "leaflet", leafletDetails);
+        const batchObject = existingBatchObjects[Math.floor(Math.random() * existingBatchObjects.length)];
+        leafletDetails.payload.productCode = batchObject.productCode;
+        leafletDetails.payload.batchNumber = batchObject.batchNumber;
+        await client.updateBatchEPI(batchObject.productCode, batchObject.batchNumber, language, "leaflet", leafletDetails);
     }
     // Test for updateBatchEPI
     console.log('Testing updateBatchEPI performance...');
-    await measureApiPerformance(updateBatchEPI);
+    await measureApiPerformance(updateBatchEPI, 20);
 
     callback();
 }, 1000000);
